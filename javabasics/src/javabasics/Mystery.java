aaad5a076c7b6d4523854645e73b817d65c337fd
ramesh097 <rameshaanji535@gmail.com>
2018-05-30 10:04:32 +0530

    sample2

---
+++ javabasics/src/javabasics/Mystery.java
@@ -0,0 +1,26 @@
+package javabasics;
+
+public class Mystery{
+	
+
+public static void main( String[] args ){
+	int y;
+	int x = 1;
+	int total = 0;
+	
+	 while ( x <= 10 )
+	 {
+	 y = x * x;
+	 System.out.println( y );
+	 total += y;
+	 ++x;
+	 } 
+
+	 System.out.printf( "Total is %d\n", total );
+	  
+	  
+}
+}
+
+
+
