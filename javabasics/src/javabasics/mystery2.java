bc2879d7c0237bd9b27da332abab8977b116e381
ramesh097 <rameshaanji535@gmail.com>
2018-05-30 10:05:47 +0530

    sample2

---
+++ javabasics/src/javabasics/mystery2.java
@@ -0,0 +1,19 @@
+package javabasics;
+
+public class mystery2 
+	
+	{
+	public static void main( String[] args )
+	{
+	int count = 1;
+
+	while ( count <= 10 )
+	{
+	 System.out.println( count % 2 == 1 ? "****" : "++++++++" );
+	 ++count;
+	 } // end while
+	 } // end main
+	 
+	}
+
+
