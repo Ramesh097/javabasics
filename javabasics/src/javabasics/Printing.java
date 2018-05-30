f99468df705a10b2aacb02a37c7b59cb9be3fe49
ramesh097 <rameshaanji535@gmail.com>
2018-05-30 10:15:16 +0530

    sample3

---
+++ javabasics/src/javabasics/Printing.java
@@ -0,0 +1,18 @@
+package javabasics;
+
+public class Printing {
+	public static void main( String[] args )
+	{
+	for ( int i = 1; i <= 10; i++ )
+	{
+	for ( int j = 1; j <= 5; j++ )
+	System.out.print( '@' );
+
+	 System.out.println();
+	 } // end outer for
+	 } // end main
+	 } // end class Printing
+
+
+
+
