e4b85ce062c1b9ff94c0a4214078c7383f0edf08
ramesh097 <rameshaanji535@gmail.com>
2018-06-01 15:26:14 +0530

    Palindrome

---
+++ javabasics/src/javabasics/Palindrome.java
@@ -0,0 +1,21 @@
+package javabasics;
+
+public class Palindrome {
+	  
+		 public static void main(String args[]){  
+		  int a,sum=0,temp;    
+		  int n=454; 
+		  
+		  temp=n;    
+		  while(n>0){    
+		   a=n%10;   
+		   sum=(sum*10)+a;    
+		   n=n/10;    
+		  }    
+		  if(temp==sum)    
+		   System.out.println("palindrome number ");    
+		  else    
+		   System.out.println("not palindrome");    
+		}  
+		}  
+
