0d508cf53ce3bf7ecaadda344cf5bb7e08182463
ramesh097 <rameshaanji535@gmail.com>
2018-06-02 15:33:27 +0530

    StudentInformation

---
+++ javabasics/src/javabasics/Student.java
@@ -0,0 +1,52 @@
+package javabasics;
+
+public class Student {
+private int id;
+private  String firstName;
+private  String lastName;
+private  int age;
+private String gender;
+ String branch;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getBranch() {
+	return branch;
+}
+public void setBranch(String branch) {
+	this.branch = branch;
+}
+@Override
+public String toString() {
+	return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", gender="
+			+ gender + ", branch=" + branch + "]";
+}
+ 
+}
