c0b48802d65fe56b1da6e2920a3ff7b4a49e2882
ramesh097 <rameshaanji535@gmail.com>
2018-06-05 17:30:53 +0530

    Sample1 problem

---
+++ javabasics/src/javabasics/Sample1.java
@@ -0,0 +1,20 @@
+package javabasics;
+
+public class Sample1 {
+	static int i= 20;
+
+public static void main(String[] args) {
+	
+
+	{ int var1 = 5; 
+	int var2 = 6;
+	if ((var2 = 1) == var1)
+	System.out.print(var2);
+	else 
+	System.out.print(++var2);
+	} 
+	}}
+
+  
+ 
+
