0294fcadde03b73fdcac1995f91b5d512c9983b6
ramesh097 <rameshaanji535@gmail.com>
2018-06-05 17:30:12 +0530

    parseDouble problem

---
+++ javabasics/src/javabasics/ParseNumber.java
@@ -0,0 +1,14 @@
+package javabasics;
+
+public class ParseNumber {
+ private static String str;
+ public static void main(String args[])
+ {
+	   double number1=Double.parseDouble(args[0]);
+     double number2=Double.parseDouble(args[1]);
+     double sum=number1+number2;
+     System.out.println("Sum :"+sum);
+     
+ }
+ 
+}
